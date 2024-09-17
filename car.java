448c8c907939698196f5655240412dcb120cc04a
Priya <priya@gmail.com>
2024-09-17 15:07:11 +0530

    sourcecode

---
+++ car.java
@@ -0,0 +1,14 @@
+package javabean;
+
+public class car {
+	private String brand;
+	public void setBrand(String brand)
+	{
+		this.brand = brand;
+	}
+	public String getBrand()
+	{
+		return brand; // return this.brand
+	}
+
+}
